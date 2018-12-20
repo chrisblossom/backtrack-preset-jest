087e396e6c9a05b164ddf30540cd1ea1ea519f3f
Chris Blossom <cbloss@gmail.com>
2018-12-20 15:22:35 -0800

    update packages

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,7 @@
 /**
  * This file is managed by backtrack
  *
- * source: @backtrack/preset-style
+ * source: @backtrack/preset-git-hooks
  * namespace: husky
  *
  * DO NOT MODIFY
@@ -14,7 +14,10 @@ const Backtrack = require('@backtrack/core');
 const { configManager } = new Backtrack();
 
 const husky = {
-    hooks: {},
+    hooks: {
+        'pre-commit': 'npm run git-pre-commit',
+        'pre-push': 'npm run git-pre-push',
+    },
 };
 
 module.exports = configManager({
