9ee5da20d68dde3fe0c14b094f3d0fcb4eb0f888
Chris Blossom <cbloss@gmail.com>
2022-05-02 16:30:09 -0700

    backtrack update

--- .backtrackrc.js
+++ .backtrackrc.js
@@ -2,4 +2,16 @@
 
 module.exports = {
 	presets: ['@backtrack/preset'],
+	config: {
+		eslint: {
+			overrides: [
+				{
+					files: ['lib/files/wallaby.js'],
+					rules: {
+						'import/extensions': 'off',
+					},
+				},
+			],
+		},
+	},
 };
